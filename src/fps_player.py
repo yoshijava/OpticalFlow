f2863e4d0cea3dd628e070085cf0037550f33f91
yoshi.huang <yoshi.huang@mediatek.com>
2016-02-24 18:09:07 +0800

    WTF

--- src/fps_player.py
+++ src/fps_player.py
@@ -25,6 +25,17 @@ scale = 0.4
 DEBUG = True
 SIMULATE_VSYNC = True
 
+# Everything we do here is treated as overheadless
+def blackhole_operation(img):
+    t1 = time.time()
+    img = cv2.resize(img, (0,0), fx=scale, fy=scale)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    t2 = time.time()
+    ch = 0xFF & cv2.waitKey(1)
+    if ch == 27:
+        return None, None
+    return (t2-t1), img
+
 def flush(msg):
     if DEBUG:
         print(msg)
@@ -44,32 +55,27 @@ if __name__ == '__main__':
     nFrames = 0
     skippedFrames = 0
     try:
-        # process the video with "raw speed", i.e., as fast as possible
+        # process the video with "raw speed" assuming it's far faster than 1/target_fps
         # if the raw speed is not fast enough, it causes "slow motion" when displaying (which is not equivalent to frame dropping)
         while True:
             t1 = time.time()
-            overhead_t1 = time.time()
             nFrames += 1
             try:
                 img = vid.get_data(i)
-                # print (t2-t1)
                 i+=1
-                img = cv2.resize(img, (0,0), fx=scale, fy=scale)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                ch = 0xFF & cv2.waitKey(1)
-                if ch == 27:
-                    break
-                overhead_t2 = time.time()
-                now = time.time();
-                period = (now - start_time)
-                if period >= (1/target_fps)-(overhead_t2-overhead_t1):
-                    cv2.imshow('FPS player', img)
-                    flush("Instant FPS = " + str(60*(nFrames - skippedFrames)/float(nFrames)))
-                    start_time = now
-                else:
-                    skippedFrames += 1
             except IndexError:
                 break
+            overhead, img = blackhole_operation(img)
+            if overhead == None:
+                break
+            now = time.time();
+            period = (now - start_time)
+            if period >= (1/target_fps)-overhead:
+                cv2.imshow('FPS player', img)
+                flush("Instant FPS = " + str(60*(nFrames - skippedFrames)/float(nFrames)))
+                start_time = now
+            else:
+                skippedFrames += 1
 
             t2 = time.time()
             elapsed = t2 - t1
--- src/opt_flow.py
+++ src/opt_flow.py
@@ -16,19 +16,18 @@ Keys:
 # Python 2/3 compatibility
 from __future__ import print_function
 
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import video
 import time
 import sys
 import math
-import pylab
 import imageio
 from subprocess import call
 
-scale = 0.3
 interval = 1
-vectorDist = [0]*35
+max_vector_dist = [0]*35
 
 def draw_flow(img, flow, step=16):
     h, w = img.shape[:2]
@@ -38,26 +37,30 @@ def draw_flow(img, flow, step=16):
     lines = np.int32(lines + 0.5)
     vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     cv2.polylines(vis, lines, 0, (0, 255, 0))
-    maxDist = 0
+    max_dist = 0
     for (x1, y1), (x2, y2) in lines:
         cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
         dist = (x1-x2)^2 + (y1-y2)^2
-        if maxDist < dist:
-            maxDist = dist
-    # if maxDist != 0:
+        if max_dist < dist:
+            max_dist = dist
+    # if max_dist != 0:
     global scale
-    sqrtMaxDist = math.sqrt(maxDist)/scale
-    # print("max dist = %d" % (sqrtMaxDist/scale)),
-    index = int(sqrtMaxDist)
-    global vectorDist
-    vectorDist[index] += 1
-    print (vectorDist)
+    sqrt_max_dist = math.sqrt(max_dist)/scale
+    # print("max dist = %d" % (sqrt_max_dist/scale)),
+    index = int(sqrt_max_dist)
+    global max_vector_dist
+    max_vector_dist[index] += 1
+    print (max_vector_dist)
     sys.stdout.flush()
     return vis
 
 if __name__ == '__main__':
     i = 1
+    if(len(sys.argv)<2):
+        print ("python opt_flow.py <<filename.mp4>> <<scale>>")
+        exit()
     filename = sys.argv[1]
+    scale = float(sys.argv[2])
     vid = imageio.get_reader(filename,  'ffmpeg')
     prev = vid.get_data(0)
     prev = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
@@ -82,9 +85,12 @@ if __name__ == '__main__':
                 )
             prev = img
             cv2.imshow('flow', draw_flow(img, flow))
+
             ch = 0xFF & cv2.waitKey(1)
             if ch == 27:
                 break
             period = (time.time() - start_time)
             # print("--- Process time: %s sec ---" % period)
     cv2.destroyAllWindows()
+    plt.plot(max_vector_dist)
+    plt.show()
