7551082a1f6512043c1e3f5943e66ddf1104edb8
yoshi.huang <yoshi.huang@mediatek.com>
2016-02-26 10:13:34 +0800

    fixed fast motion

--- src/fps_player.py
+++ src/fps_player.py
@@ -46,7 +46,9 @@ if __name__ == '__main__':
     try:
         # process the video with "raw speed" assuming it's far faster than 1/target_fps
         # if the raw speed is not fast enough, it causes "slow motion" when displaying (which is not equivalent to frame dropping)
+        wall_clock_t1 = time.time()
         while True:
+            t1 = time.time()
             try:
                 img = vid.get_data(int(i))
                 i += 60/target_fps
@@ -63,6 +65,10 @@ if __name__ == '__main__':
             cv2.imshow('FPS player', img)
             last_refresh = new_refresh
             new_refresh = time.time()
+            t2 = time.time()
+            if (t2-t1) < 1/target_fps:
+                time.sleep( 1/target_fps - (t2-t1))
+
             if SIMULATE_VSYNC:
                 vsync_overhead_start = time.time()
                 if new_refresh - last_refresh <= vsync_signal :
@@ -76,8 +82,9 @@ if __name__ == '__main__':
                 else:
                     flush("Processing speed requires > 1/60. The display may be a bit slow-motion but still correct for smoothness")
 
-
+        wall_clock_t2 = time.time()
     except KeyboardInterrupt:
         print ("Interrupted by user...")
 
     cv2.destroyAllWindows()
+    print("wall clock elapsed: ", (wall_clock_t2-wall_clock_t1), " sec")
